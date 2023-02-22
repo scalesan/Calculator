d8e0d0c062a970a35828a6327ef63074b8d6d536
Anthony Scales <anthony,scalesii@gmail.com>
2023-02-22 11:02:40 -0500

    Added some styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
+  const createMyDigits = () => {
+    const myDigits = [];
+    for(let i=1; i<10; i++){
+      myDigits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return myDigits
+  }
   return (
     <div className="App">
-      Hello World
+        <div className="myCalc">
+          <div className="myDisplay">
+            <span>{0}</span>0
+          </div>
+        <div className = "myOperators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+
+          <button>DEL</button>
+        </div>
+        <div className="myDigits">
+          {createMyDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
     </div>
+  </div>
   );
 }
 
